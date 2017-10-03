32f4899f0baee79b409263c8f682823d1448587f
Rodrigo Lima <rlo@cin.ufpe.br>
2017-10-02 23:19:14 -0300

    Consertando alguns erros e adicionando 2 e 3

--- ProvaPLC2017.hs
+++ ProvaPLC2017.hs
@@ -12,4 +12,36 @@ valorDiag [[]] = []
 valorDiag ((x:xs):ys) = [x] ++ valorDiag (map (drop 1) ys)
 
 troca :: [[Int]] -> Int -> Int -> [[Int]]
-troca x y l = init (take x l) ++ [(take y l)!!(y-1)] ++ (take(x-y-1) (drop x l)) ++ [(take x l) !! (x-1)] ++ (drop y l)
+troca l x y = (take (x-1) l) ++(take 1(drop (y-1) l)) ++ (take(y-x-1)(drop x l)) ++ (take 1(drop (x-1) l)) ++ (drop y l)
+
+type Codigo = Int
+data Voto = Presidente Codigo | Conselheiro Codigo | Secretario Codigo | Branco deriving (Show)
+type Urna = [Voto]
+type Apuraçao = [(Voto, Int)]
+instance Eq Voto where
+  (Presidente a)==(Presidente b) = a==b
+  (Conselheiro a)==(Conselheiro b) = a==b
+  (Secretario a)==(Secretario b) = a==b
+  (Branco)==(Branco) = True
+  _== _ = False
+
+totalVotos :: Urna -> Voto -> Int
+totalVotos a b = length([x | x <-a, x==b])
+
+apurar :: Urna -> Apuraçao
+apurar[] = []
+apurar l@(x:xs) = (x, totalVotos l x) : apurar (filter (/=x) xs)
+
+data Pilha t = Pilha [t] | PilhaVazia deriving (Show)
+
+push :: t -> Pilha t -> Pilha t
+push t PilhaVazia = Pilha [t]
+push t (Pilha a) = (Pilha (t:a))
+
+pop :: Pilha t -> Pilha t
+pop PilhaVazia = error "Pilha Vazia"
+pop (Pilha (x:xs)) = (Pilha xs)
+
+top :: Pilha t -> t
+top PilhaVazia = error "Pilha Vazia"
+top (Pilha (x:xs)) = x
